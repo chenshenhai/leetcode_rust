058d819c9e5f1f7ad4b6d6f2553e2569f84a5b8a
chenshenhai <cshenhai@gmail.com>
2020-02-16 00:04:07 +0800

    feat: update valid_parentheses

--- easy/valid_parentheses/src/main.rs
+++ easy/valid_parentheses/src/main.rs
@@ -38,44 +38,41 @@ struct Solution {}
 impl Solution {
     pub fn is_valid(s: String) -> bool {
         let mut result = true;
-        // let min_start = 40; // 小括号开始 "("
-        // let min_end = 41; // 小括号闭合  ")"
-        // let mid_start = 91; // 中括号开始 "["
-        // let mid_end = 93; // 中括号闭合 "]"
-        // let max_start = 123; // 中括号开始 "{"
-        // let max_end = 125; // 中括号闭合 "}"
-
-        let mut min_count = 0;
-        let mut mid_count = 0;
-        let mut max_count = 0;
+        let min_start = 40; // 小括号开始 "("
+        let min_end = 41; // 小括号闭合  ")"
+        let mid_start = 91; // 中括号开始 "["
+        let mid_end = 93; // 中括号闭合 "]"
+        let max_start = 123; // 中括号开始 "{"
+        let max_end = 125; // 中括号闭合 "}"
 
         let str_bytes = s.as_bytes();
         let stack = std::vec::Vec::new();
 
         for i in 0..str_bytes.len() as usize {
-            // match str_bytes[i] {
-            //     40 => min_count += 1, 
-            //     41 => min_count -= 1, 
-            //     91 => mid_count += 1, 
-            //     93 => mid_count -= 1, 
-            //     123 => max_count += 1, 
-            //     125 => max_count -= 1, 
-            //     _=> continue,
-            // }
-            if str_bytes[i] == 40 || str_bytes[i] == 91 || str_bytes[i] == 123 {
-                if str_bytes[i] == 40 {
-                    stack.push(41);
-                } else if str_bytes[i] == 91 {
-                    stack.push(93);
-                } if str_bytes[i] == 123 {
-                    stack.push(125);
+            if str_bytes[i] == min_start || str_bytes[i] == mid_start || str_bytes[i] == max_start {
+                if str_bytes[i] == min_start {
+                    stack.push(min_end);
+                } else if str_bytes[i] == mid_start {
+                    stack.push(mid_end);
+                } if str_bytes[i] == max_start {
+                    stack.push(max_end);
                 }
             } else {
-                
+                let out = stack.pop();
+                if out == min_end && str_bytes[i] == min_end {
+                    continue;
+                } else if out == mid_end && str_bytes[i] == mid_end {
+                    continue; 
+                } else if out == max_end && str_bytes[i] == max_end {
+                    continue; 
+                } else {
+                    result = false;
+                    break;
+                }
             }
         }
 
-        if min_count > 0 || mid_count > 0 || max_count > 0 {
+        if stack.len() > 0 {
             result = false;
         } 
 
