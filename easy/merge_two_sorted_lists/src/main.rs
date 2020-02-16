65ba9d87350f313b3e0d2816857462eb90fb6f8d
chenshenhai <cshenhai@gmail.com>
2020-02-16 14:53:03 +0800

    feat: update demo merge_two_sorted_lists

--- easy/merge_two_sorted_lists/src/main.rs
+++ easy/merge_two_sorted_lists/src/main.rs
@@ -13,28 +13,54 @@
 */
 
 // Definition for singly-linked list.
-// #[derive(PartialEq, Eq, Clone, Debug)]
-// pub struct ListNode {
-//   pub val: i32,
-//   pub next: Option<Box<ListNode>>
-// }
-// 
-// impl ListNode {
-//   #[inline]
-//   fn new(val: i32) -> Self {
-//     ListNode {
-//       next: None,
-//       val
-//     }
-//   }
-// }
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+  pub val: i32,
+  pub next: Option<Box<ListNode>>
+}
 
+impl ListNode {
+  #[inline]
+  fn new(val: i32) -> Self {
+    ListNode {
+      next: None,
+      val
+    }
+  }
+}
+
+pub struct Solution {}
 impl Solution {
     pub fn merge_two_lists(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-        
+        // TODO
+        let result = create_link(1, 8, 1);
+        result
     }
 }
 
+fn create_link(start: i32, count: i32, unit: i32) -> Option<Box<ListNode>> {
+    let mut head_node = Some(Box::new(ListNode::new(start)));
+    let mut prev_node = &mut head_node;
+    for i in 1..count as usize {
+        match prev_node {
+            Some(node) => {
+                let idx = i as i32;
+                node.next = Some(Box::new(ListNode::new(idx * unit)));
+            },
+            None => {},
+        }
+    }
+    head_node
+}
+
 fn main() {
-    println!("Hello, world!");
+    // 1->2->4, 1->3->4
+    let l1 = create_link(1, 4, 2); // 1->3->5->7
+    let l2 = create_link(2, 4, 2); // 2->4->6->8
+    let expect = create_link(1, 8, 1); // 1->2->3->4->5->6->7->8
+    let result = Solution::merge_two_lists(l1, l2);
+    assert_eq!(format!("{:?}", result), format!("{:?}", expect));
+ 
+    
+    println!("success!");
 }
